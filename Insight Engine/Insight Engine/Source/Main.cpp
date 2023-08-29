1fd744742e5686d0f31fdc8b16b53f62591195e8
tzx2110 <t.zhengxun@digipen.edu>
2023-08-30 04:05:33 +0800

    up

---
+++ Insight Engine/Insight Engine/Source/Main.cpp
@@ -0,0 +1,49 @@
+///////////////////////////////////////////////////////////////////////////////////////
+//
+//	Main
+//	The main entry point for the game--everything starts here.
+//	
+//	Authors:  Tan Zheng Xun
+//	Copyright 2023, Digipen Institute of Technology
+//
+///////////////////////////////////////////////////////////////////////////////////////
+
+#define WIDTH 1280
+#define HEIGHT 720
+#include <GLFW/glfw3.h>
+
+int main(void)
+{
+    GLFWwindow* window;
+
+    /* Initialize the library */
+    if (!glfwInit())
+        return -1;
+
+    /* Create a windowed mode window and its OpenGL context */
+    window = glfwCreateWindow(WIDTH, HEIGHT, "Hello World", NULL, NULL);
+    if (!window)
+    {
+        glfwTerminate();
+        return -1;
+    }
+
+    /* Make the window's context current */
+    glfwMakeContextCurrent(window);
+
+    /* Loop until the user closes the window */
+    while (!glfwWindowShouldClose(window))
+    {
+        /* Render here */
+        glClear(GL_COLOR_BUFFER_BIT);
+
+        /* Swap front and back buffers */
+        glfwSwapBuffers(window);
+
+        /* Poll for and process events */
+        glfwPollEvents();
+    }
+
+    glfwTerminate();
+    return 0;
+}
