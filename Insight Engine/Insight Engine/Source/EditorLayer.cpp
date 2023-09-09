c42f82c9da0ae938ea2f3e1434d7d3b3c342d52c
ilovebnc <guoyiming340@gmail.com>
2023-09-09 21:17:18 +0800

    minor changes to performance viewer layout and added logging window (hooking with our logs in the future, hopefully soon)

--- Insight Engine/Insight Engine/Source/EditorLayer.cpp
+++ Insight Engine/Insight Engine/Source/EditorLayer.cpp
@@ -3,11 +3,6 @@
 #include "Graphics.h"
 #include "CoreEngine.h"
 
-// Dependencies
-#include <imgui.h>
-#include <imgui_impl_glfw.h>
-#include <imgui_impl_opengl3.h>
-
 namespace IS {
 
     bool show_another_window = false;
@@ -121,6 +116,7 @@ namespace IS {
         // Other windows
         RenderInspector();
         RenderPerformanceViewer();
+        RenderLogConsole();
 
         ImGui::End();
     }
@@ -148,15 +144,61 @@ namespace IS {
     void EditorLayer::RenderPerformanceViewer() {
         ImGuiIO& io = ImGui::GetIO();
         ImGui::Begin("Performance");
-        ImGui::Text("%.0f FPS", io.Framerate);
-        ImGui::Text("%.2f ms", 1000.f / io.Framerate);
-        for (auto& [system, dt] : InsightEngine::Instance().systemDeltas) {
-            double percent = std::round((dt / InsightEngine::Instance().systemDeltas["Engine"]) * 100.0);
-            if (system != "Engine")
-                ImGui::Text("%3d%% %s", static_cast<int>(percent), system.c_str());
-        }
+        ImGui::Text("Framerate: %.0f FPS", io.Framerate);
+        ImGui::Text("Timestep: %.2f ms", 1000.f / io.Framerate);
+        ImGui::Dummy({ 5.f, 5.f });
+        ImGui::Separator();
+        ImGui::Dummy({ 5.f, 5.f });
         
+        if (ImGui::BeginTable("Systems", 2, ImGuiTableFlags_Resizable, ImVec2(0.f, 0.f))) {
+            ImGui::TableSetupColumn("System");
+            ImGui::TableSetupColumn("Usage %");
+            ImGui::TableHeadersRow();
+            for (auto& [system, dt] : InsightEngine::Instance().systemDeltas) {
+                double percent = std::round((dt / InsightEngine::Instance().systemDeltas["Engine"]) * 100.0);
+                if (system != "Engine") {
+                    ImGui::TableNextColumn();
+                    ImGui::Spacing();
+                    ImGui::Text("%s", system.c_str());
+                    ImGui::Separator();
+
+                    ImGui::TableNextColumn();
+                    ImGui::Spacing();
+                    ImGui::Text("%3d%%", static_cast<int>(percent));
+                    ImGui::Separator();
+                }
+            }
+            ImGui::EndTable();
+        }
+
         ImGui::End();
+    }
+
+    // From imgui demo (for reference)
+    void EditorLayer::RenderLogConsole() {
+        static ExampleAppLog log;
+
+        // For the demo: add a debug button _BEFORE_ the normal log window contents
+        // We take advantage of a rarely used feature: multiple calls to Begin()/End() are appending to the _same_ window.
+        // Most of the contents of the window will be added by the log.Draw() call.
+        ImGui::SetNextWindowSize(ImVec2(500, 400), ImGuiCond_FirstUseEver);
+        ImGui::Begin("Log");
+        if (ImGui::SmallButton("[Debug] Add 5 entries")) {
+            static int counter = 0;
+            const char* categories[6] = { "trace", "info", "debug", "warn", "error", "critical" };
+            const char* words[] = { "Bumfuzzled", "Cattywampus", "Snickersnee", "Abibliophobia", "Absquatulate", "Nincompoop", "Pauciloquent" };
+            for (int n = 0; n < 5; n++) {
+                const char* category = categories[counter % IM_ARRAYSIZE(categories)];
+                const char* word = words[counter % IM_ARRAYSIZE(words)];
+                log.AddLog("[%05d] [%s] Hello, current time is %.1f, here's a word: '%s'\n",
+                           ImGui::GetFrameCount(), category, ImGui::GetTime(), word);
+                counter++;
+            }
+        }
+        ImGui::End();
+
+        // Actually call in the regular Log helper (which will Begin() into the same window as we just did)
+        log.Draw("Log");
     }
 
 } // end namespace IS
