38b3bf589383d71f67147fa0d306af75eda3d3c3
YankhangSIT <112969742+YankhangSIT@users.noreply.github.com>
2023-09-15 11:56:24 +0800

    4zk

---
+++ Insight Engine/Insight Engine/Source/Transform.cpp
@@ -0,0 +1,32 @@
+#include "Pch.h"
+#include "Transform.h"
+
+
+namespace IS {
+	Vector2D Transform::getWorldPosition() {
+		return Vector2D(world_position.x, world_position.y);
+	}
+
+	Vector2D Transform::getOrientation() {
+		return Vector2D(orientation.x, world_position.y);
+	}
+
+	Vector2D Transform::getScaling() {
+		return Vector2D(scaling.x, scaling.y);
+	}
+
+	void Transform::setWorldPosition(float x, float y) {
+		world_position.x = x;
+		world_position.y = y;
+	}
+
+	void Transform::setOrientation(float angle, float angle_speed) {
+		orientation.x = angle;
+		orientation.y = angle_speed;
+	}
+
+	void Transform::setScaling(float width, float height) {
+		scaling.x = width;
+		scaling.y = height;
+	}
+}
---
+++ Insight Engine/Insight Engine/Source/Transform.h
@@ -0,0 +1,22 @@
+#pragma once
+#include "CoreEngine.h"
+#include <glm/glm.hpp>
+
+namespace IS {
+	class Transform : public IComponent {
+	public:
+		// transformation
+		glm::vec2 world_position{}; // world coordinates (if world is 1280 pixels, valid world positions: [-640, 640]
+		glm::vec2 orientation{};   // x = angle displacement, y = angle speed
+		glm::vec2 scaling{}; // x = width, y = height
+		glm::mat3 mdl_to_ndl_xform = glm::imat3x3{};
+
+		Vector2D getWorldPosition();
+		Vector2D getOrientation();
+		Vector2D getScaling();
+
+		void setWorldPosition(float x, float y);
+		void setOrientation(float angle, float angle_speed);
+		void setScaling(float width, float height);
+	};
+}
