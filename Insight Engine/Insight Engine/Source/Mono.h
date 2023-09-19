b2a48f7ebd3b15433fe500f1171783a6e381df97
macoki07 <70043062+macoki07@users.noreply.github.com>
2023-09-19 23:28:10 +0800

    i have made the dll for mono work

--- Insight Engine/Insight Engine/Source/Mono.h
+++ Insight Engine/Insight Engine/Source/Mono.h
@@ -3,5 +3,16 @@
 #include <mono/metadata/assembly.h>
 
 void init() {
-    mono_set_assemblies_path("mono/lib");
+    mono_set_assemblies_path("libraries/mono/lib/4.5");
+
+    MonoDomain* rootDomain = mono_jit_init("MyScriptRuntime");
+    if (rootDomain == nullptr)
+    {
+        // Maybe log some error here
+        return;
+    }
+
+    // Store the root domain pointer
+    MonoDomain*  s_RootDomain = rootDomain;
+
 }
