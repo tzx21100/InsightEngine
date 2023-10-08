221666804c491290183c0b43cc6911d57008392b
ilovebnc <guoyiming340@gmail.com>
2023-10-08 16:05:35 +0800

    new scene in GUI

--- Insight Engine/Insight Engine/Source/Logger.h
+++ Insight Engine/Insight Engine/Source/Logger.h
@@ -54,16 +54,46 @@ namespace IS {
         Critical
     };
 
-    // Simple thread-safe logger
+    /**
+     * @class Logger
+     * @brief A flexible logging class that allows different log levels and output options.
+     *
+     * The `Logger` class provides a versatile logging system with support for different log levels,
+     * timestamp formatting, and optional file output. It also includes a GUI interface for real-time log viewing.
+     */
     class Logger {
     public:
-
+        /**
+         * @class LoggerGUI
+         * @brief GUI interface for real-time log viewing.
+         *
+         * The `LoggerGUI` class provides an ImGui-based GUI for viewing and managing log messages in real-time.
+         */
         class LoggerGUI {
         public:
+            /**
+             * @brief Constructor for the LoggerGUI class.
+             */
             LoggerGUI();
 
+            /**
+             * @brief Clear the log messages in the GUI.
+             */
             void Clear();
+
+            /**
+             * @brief Add a log message to the GUI.
+             *
+             * @param fmt The log message format.
+             * @param ... Additional log message arguments.
+             */
             void AddLog(const char* fmt, ...);
+
+            /**
+             * @brief Draw the GUI window for log viewing.
+             *
+             * @param title The title of the log window.
+             */
             void Draw(const char* title);
             //ImU32 getLogLevelColor(aLogLevel log_level);
 
@@ -77,33 +107,150 @@ namespace IS {
         };
 
         // Constructor/Destructor
+
+        /**
+         * @brief Constructor for the Logger class.
+         *
+         * @param name The name of the logger.
+         */
         Logger(std::string const& name = "");
+
+        /**
+         * @brief Destructor for the Logger class.
+         */
         ~Logger();
 
-        // Log functions
+        /**
+         * @brief Log a message with the Trace log level.
+         *
+         * This function is used to log a message with the Trace log level. It allows you to include
+         * log message formatting and arguments.
+         *
+         * @param fmt The log message format.
+         * @param args Additional log message arguments.
+         */
         template<typename... Args>
         void Trace(std::string_view fmt, Args&&... args);
+
+        /**
+         * @brief Log a message with the Debug log level.
+         *
+         * This function is used to log a message with the Debug log level. It allows you to include
+         * log message formatting and arguments.
+         *
+         * @param fmt The log message format.
+         * @param args Additional log message arguments.
+         */
         template<typename... Args>
         void Debug(std::string_view fmt, Args&&... args);
+
+        /**
+         * @brief Log a message with the Info log level.
+         *
+         * This function is used to log a message with the Info log level. It allows you to include
+         * log message formatting and arguments.
+         *
+         * @param fmt The log message format.
+         * @param args Additional log message arguments.
+         */
         template<typename... Args>
         void Info(std::string_view fmt, Args&&... args);
+
+        /**
+         * @brief Log a message with the Warning log level.
+         *
+         * This function is used to log a message with the Warning log level. It allows you to include
+         * log message formatting and arguments.
+         *
+         * @param fmt The log message format.
+         * @param args Additional log message arguments.
+         */
         template<typename... Args>
         void Warn(std::string_view fmt, Args&&... args);
+
+        /**
+         * @brief Log a message with the Error log level.
+         *
+         * This function is used to log a message with the Error log level. It allows you to include
+         * log message formatting and arguments.
+         *
+         * @param fmt The log message format.
+         * @param args Additional log message arguments.
+         */
         template<typename... Args>
         void Error(std::string_view fmt, Args&&... args);
+
+        /**
+         * @brief Log a message with the Critical log level.
+         *
+         * This function is used to log a message with the Critical log level. It allows you to include
+         * log message formatting and arguments.
+         *
+         * @param fmt The log message format.
+         * @param args Additional log message arguments.
+         */
         template<typename... Args>
         void Critical(std::string_view fmt, Args&&... args);
 
-        // Getter/setter
+        /**
+         * @brief Set the name of the logger.
+         *
+         * This function sets the name of the logger, which can be used for identification or labeling purposes.
+         *
+         * @param new_logger_name The new name for the logger.
+         */
         void SetLoggerName(std::string const& new_logger_name);
+
+        /**
+         * @brief Set the log level for the logger.
+         *
+         * This function sets the log level for the logger, controlling which log messages are displayed.
+         *
+         * @param new_level The new log level.
+         */
         void SetLogLevel(aLogLevel new_level);
+
+        /**
+         * @brief Get the current log level of the logger.
+         *
+         * This function returns the current log level of the logger, indicating the level of log messages
+         * that are being logged and displayed.
+         *
+         * @return The current log level.
+         */
         aLogLevel GetLogLevel() const;
+
+        /**
+         * @brief Set the timestamp format for log messages.
+         *
+         * This function sets the format for timestamping log messages. You can customize the timestamp format
+         * to suit your needs.
+         *
+         * @param new_timestamp_format The new timestamp format.
+         */
         void SetTimestampFormat(std::string const& new_timestamp_format);
+
+        /**
+         * @brief Get the current timestamp format for log messages.
+         *
+         * This function returns the current timestamp format used for log messages.
+         *
+         * @return The current timestamp format.
+         */
         std::string GetTimestampFormat() const;
 
         // Additional options
+
+        /**
+         * @brief Enable file output for log messages.
+         */
         void EnableFileOutput();
 
+        /**
+         * @brief Get the LoggerGUI instance for log viewing.
+         *
+         * @return A reference to the LoggerGUI instance.
+         */
         static LoggerGUI& GetLoggerGUI();
 
     private:
@@ -119,47 +266,64 @@ namespace IS {
 
         static std::mutex mLogMutex;
 
+        /**
+         * @brief Log a message with the specified log level.
+         *
+         * @param level The log level.
+         * @param fmt The log message format.
+         * @param args Additional log message arguments.
+         */
         template<typename... Args>
         void log(aLogLevel level, std::string_view fmt, Args&&... args);
 
+        /**
+         * @brief Get a timestamp with the specified format.
+         *
+         * @param ts_format The timestamp format.
+         * @return The formatted timestamp.
+         */
         std::string getTimestamp(std::string const& ts_format) const;
+
+        /**
+         * @brief Close the log file (if open).
+         */
         void closeFile();
 
         // Static functions
+
+        /**
+         * @brief Get a string representation of a log level.
+         *
+         * @param level The log level.
+         * @return The log level as a string.
+         */
         static std::string getLogLevelString(aLogLevel level);
+
+        /**
+         * @brief Set the color for displaying log messages of a specific level.
+         *
+         * @param level The log level.
+         */
         static void setColor(aLogLevel level);
     };
 
-    // Template implementation
     template<typename... Args>
-    inline void Logger::Trace(std::string_view fmt, Args&&... args) {
-        log(aLogLevel::Trace, fmt, args...);
-    }
+    inline void Logger::Trace(std::string_view fmt, Args&&... args) { log(aLogLevel::Trace, fmt, args...); }
 
     template<typename... Args>
-    inline void Logger::Debug(std::string_view fmt, Args&&... args) {
-        log(aLogLevel::Debug, fmt, args...);
-    }
+    inline void Logger::Debug(std::string_view fmt, Args&&... args) { log(aLogLevel::Debug, fmt, args...); }
 
     template<typename... Args>
-    inline void Logger::Info(std::string_view fmt, Args&&... args) {
-        log(aLogLevel::Info, fmt, args...);
-    }
+    inline void Logger::Info(std::string_view fmt, Args&&... args) { log(aLogLevel::Info, fmt, args...); }
 
     template<typename... Args>
-    inline void Logger::Warn(std::string_view fmt, Args&&... args) {
-        log(aLogLevel::Warning, fmt, args...);
-    }
+    inline void Logger::Warn(std::string_view fmt, Args&&... args) { log(aLogLevel::Warning, fmt, args...); }
 
     template<typename... Args>
-    inline void Logger::Error(std::string_view fmt, Args&&... args) {
-        log(aLogLevel::Error, fmt, args...);
-    }
+    inline void Logger::Error(std::string_view fmt, Args&&... args) { log(aLogLevel::Error, fmt, args...); }
 
     template<typename... Args>
-    inline void Logger::Critical(std::string_view fmt, Args&&... args) {
-        log(aLogLevel::Critical, fmt, args...);
-    }
+    inline void Logger::Critical(std::string_view fmt, Args&&... args) { log(aLogLevel::Critical, fmt, args...); }
 
     template<typename... Args>
     inline void Logger::log(aLogLevel level, std::string_view fmt, Args&&... args) {
@@ -180,9 +344,8 @@ namespace IS {
             std::clog << log.str() << RESET;
 
             // Write to file
-            if (mLogFile.is_open()) {
+            if (mLogFile.is_open())
                 mLogFile << log.str();
-            }
 
             mLoggerGUI.AddLog(log.str().c_str());
         }
