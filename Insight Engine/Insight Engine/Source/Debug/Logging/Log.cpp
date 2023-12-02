b3e227aab943605c0adc7777d1054bf76502fd2b
ilovebnc <113174365+ilovebnc@users.noreply.github.com>
2023-12-02 12:53:55 +0800

    debug folder

---
+++ Insight Engine/Insight Engine/Source/Debug/Logging/Log.cpp
@@ -0,0 +1,43 @@
+/*!
+ * \file Log.cpp
+ * \author Guo Yiming, yiming.guo@digipen.edu
+ * \par Course: CSD2401
+ * \date 23-09-2023
+ * \brief
+ * This source file defines the implementation for class Log which encapsulates
+ * the functionalities of an Engine-side log and a Client-side log.
+ * 
+ * \copyright
+ * All content (C) 2023 DigiPen Institute of Technology Singapore.
+ * All rights reserved.
+ * Reproduction or disclosure of this file or its contents without the prior written
+ * consent of DigiPen Institute of Technology is prohibited.
+ *____________________________________________________________________________*/
+
+/*                                                                   includes
+----------------------------------------------------------------------------- */
+#include "Pch.h"
+#include "Log.h"
+
+namespace IS {
+
+    // Static instances
+    std::shared_ptr<Logger> Log::mCoreLogger;
+    std::shared_ptr<Logger> Log::mClientLogger;
+
+    void Log::Init(bool enable_output)
+    {
+        mCoreLogger = std::make_shared<Logger>("Insight");
+        mClientLogger = std::make_shared<Logger>("Fragments");
+
+        mCoreLogger->SetLogLevel(aLogLevel::LOGLEVEL_TRACE);
+        mClientLogger->SetLogLevel(aLogLevel::LOGLEVEL_TRACE);
+
+        if (enable_output)
+        {
+            mCoreLogger->EnableFileOutput();
+            mClientLogger->EnableFileOutput();
+        }
+    }
+
+} // end namespace IS
