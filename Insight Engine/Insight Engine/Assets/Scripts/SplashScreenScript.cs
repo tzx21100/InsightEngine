12081c06e6dfce773ef0feaf5ed071438ecf8490
macoki07 <70043062+macoki07@users.noreply.github.com>
2024-01-23 09:40:08 +0800

    splish splash

--- Insight Engine/Insight Engine/Assets/Scripts/SplashScreenScript.cs
+++ Insight Engine/Insight Engine/Assets/Scripts/SplashScreenScript.cs
@@ -31,12 +31,14 @@ namespace IS
         static Vector2D origin = new Vector2D(0, 0);
 
         static private int team_logo_entity;
+        static private int splash_entity;
         static private int top_left_splash_entity;
         static private int top_right_splash_entity;
         static private int bottom_left_splash_entity;
         static private int bottom_right_splash_entity;
 
         static SimpleImage team_logo_image;
+        static SimpleImage splash_image;
         static SimpleImage top_left_splash_image;
         static SimpleImage top_right_splash_image;
         static SimpleImage bottom_left_splash_image;
@@ -46,6 +48,7 @@ namespace IS
         static private float jigsaw_scale_y;
 
         static Vector2D team_logo_pos = new Vector2D(0, 0);
+        static Vector2D splash_pos = new Vector2D(0, 0);
         static Vector2D top_left_splash_pos = new Vector2D(0, 0);
         static Vector2D top_right_splash_pos = new Vector2D(0, 0);
         static Vector2D bottom_left_splash_pos = new Vector2D(0, 0);
@@ -75,12 +78,14 @@ namespace IS
             top_right_splash_image = InternalCalls.GetSpriteImage("splash_top_right.png");
             bottom_left_splash_image = InternalCalls.GetSpriteImage("splash_bottom_left.png");
             bottom_right_splash_image = InternalCalls.GetSpriteImage("splash_bottom_right.png");
+            splash_image = InternalCalls.GetSpriteImage("main_menu_bg.jpeg");
             team_logo_image = InternalCalls.GetSpriteImage("team_logo.png");
 
             top_left_splash_entity = InternalCalls.CreateEntityUI("Top Left", top_left_splash_image);
             top_right_splash_entity = InternalCalls.CreateEntityUI("Top Right", top_right_splash_image);
             bottom_left_splash_entity = InternalCalls.CreateEntityUI("Bottom Left", bottom_left_splash_image);
             bottom_right_splash_entity = InternalCalls.CreateEntityUI("Bottom Right", bottom_right_splash_image);
+            splash_entity = InternalCalls.CreateEntityUI("Splash", splash_image);
             team_logo_entity = InternalCalls.CreateEntityUI("Team Logo", team_logo_image);
 
             InternalCalls.SetSpriteAlphaEntity(0f, team_logo_entity);
@@ -114,14 +119,17 @@ namespace IS
             InternalCalls.TransformSetScaleEntity(jigsaw_scale_x, jigsaw_scale_y, top_right_splash_entity);
             InternalCalls.TransformSetScaleEntity(jigsaw_scale_x, jigsaw_scale_y, bottom_left_splash_entity);
             InternalCalls.TransformSetScaleEntity(jigsaw_scale_x, jigsaw_scale_y, bottom_right_splash_entity);
+            InternalCalls.TransformSetScaleEntity(win_dimension.x, win_dimension.y  , splash_entity);
             InternalCalls.TransformSetScaleEntity(win_dimension.x/2, win_dimension.y/2, team_logo_entity);
 
             top_left_splash_pos.Set(origin.x + (win_dimension.x * 0.262f), origin.y + (win_dimension.y * 0.698f)); //-455.14, 214.43
             top_right_splash_pos.Set(origin.x + (win_dimension.x * 0.727f), origin.y + (win_dimension.y * 0.698f)); //437.59, 214.43
             bottom_left_splash_pos.Set(origin.x + (win_dimension.x * 0.268f), origin.y + (win_dimension.y * 0.279f)); //-449.29, -237.83
             bottom_right_splash_pos.Set(origin.x + (win_dimension.x * 0.731f), origin.y + (win_dimension.y * 0.294f)); //444.9. -221.74
+            splash_pos.Set(origin.x + (win_dimension.x * 0.5f), origin.y + (win_dimension.y * 0.5f));
             team_logo_pos.Set(origin.x + (win_dimension.x * 0.5f), origin.y + (win_dimension.y * 0.5f));
 
+            HideJigsaw();
             if (jigsaw_animation_timer > 0)
             {
                 float progress = (jigsaw_animation_duration - jigsaw_animation_timer) / jigsaw_animation_duration;
@@ -134,11 +142,17 @@ namespace IS
 
                 // Update timer
                 jigsaw_animation_timer -= dt;
+
             }
-            DrawJigsaw();
+            DrawJigsawPieces();
             DrawLogo();
 
             timer += dt;
+            if(timer >= 2)
+            {
+                HideJigsawPieces();
+                DrawSplash();
+            }
             if (timer >= 2 && logo_animation_timer > 0)
             {
                 float alpha;
@@ -172,12 +186,27 @@ namespace IS
 
         }
 
-        static private void DrawJigsaw()
+        static private void DrawJigsawPieces()
         {
             InternalCalls.TransformSetPositionEntity(top_left_splash_pos.x, top_left_splash_pos.y, top_left_splash_entity);
             InternalCalls.TransformSetPositionEntity(top_right_splash_pos.x, top_right_splash_pos.y, top_right_splash_entity);
             InternalCalls.TransformSetPositionEntity(bottom_left_splash_pos.x, bottom_left_splash_pos.y, bottom_left_splash_entity);
             InternalCalls.TransformSetPositionEntity(bottom_right_splash_pos.x, bottom_right_splash_pos.y, bottom_right_splash_entity);
+        }
+        static private void HideJigsawPieces()
+        {
+            InternalCalls.TransformSetPositionEntity(9999f, 9999f, top_left_splash_entity);
+            InternalCalls.TransformSetPositionEntity(9999f,9999f, top_right_splash_entity);
+            InternalCalls.TransformSetPositionEntity(9999f, 9999f, bottom_left_splash_entity);
+            InternalCalls.TransformSetPositionEntity(9999f, 9999f, bottom_right_splash_entity);
+        }
+        static private void DrawSplash()
+        {
+            InternalCalls.TransformSetPositionEntity(splash_pos.x, splash_pos.y, splash_entity);
+        }
+        static private void HideJigsaw()
+        {
+            InternalCalls.TransformSetPositionEntity(9999f, 9999f, splash_entity);
         }
 
         static private void DrawLogo()
