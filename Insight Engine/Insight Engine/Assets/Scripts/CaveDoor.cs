6c5a7a2748e44a3bda4497499bfe0e56d3c70451
tzx21100 <115536100+tzx21100@users.noreply.github.com>
2024-01-26 22:11:41 +0800

    aaaa

---
+++ Insight Engine/Insight Engine/Assets/Scripts/CaveDoor.cs
@@ -0,0 +1,19 @@
+using System.Runtime.CompilerServices;
+namespace IS
+{
+    class CaveDoor
+    {
+        static public void Init(){
+
+        }
+
+        static public void Update(){
+            
+        }
+        
+        static public void CleanUp(){
+
+        }
+
+    }
+}
