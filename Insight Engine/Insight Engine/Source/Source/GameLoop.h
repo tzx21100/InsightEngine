a5f1d2bed36c483bdb5717e16728dfde3cf76844
ilovebnc <guoyiming340@gmail.com>
2024-02-07 19:41:12 +0800

    dk why never get committed

--- Insight Engine/Insight Engine/Source/Source/GameLoop.h
+++ Insight Engine/Insight Engine/Source/Source/GameLoop.h
@@ -258,7 +258,7 @@ namespace IS {
 
                     if (!engine.mRenderGUI)
                     {
-                        window_sys->LoadCustomCursor();
+                        window_sys->UseCustomCursor();
                     }
                 }
 
