30cd0afa04152eb21ea731ac9abf98055b8c936b
YankhangSIT <112969742+YankhangSIT@users.noreply.github.com>
2024-02-09 00:46:39 +0800

    new readme + documentation

--- Insight Engine/Insight Engine/Source/Graphics/System/ShaderEffects.h
+++ Insight Engine/Insight Engine/Source/Graphics/System/ShaderEffects.h
@@ -27,6 +27,13 @@
 namespace IS {
     class ShaderEffect {
     public:
+        /*!
+         * \enum shaderEffect
+         * \brief Enumerates the types of shader effects that can be applied.
+         *
+         * This enumeration lists the various shader effects supported by the ShaderEffect class,
+         * allowing for easy identification and switching between effects.
+         */
         enum class shaderEffect {
             no_effect,
             dimmed,
@@ -36,11 +43,36 @@ namespace IS {
         };
 
         
-
+        /*!
+         * \brief Applies a static glitch effect to the current rendering scene.
+         *
+         * This static method activates the static glitch shader effect, modifying the rendering
+         * to simulate a glitchy visual appearance. The effect is applied by setting the currentShaderEffect
+         * to static_glitch. This method is intended for use when a visual disruption or glitch-like
+         * animation is desired in the scene.
+         */
         static void play_static_glitch();
-        static void play_fragment_light();
-        static void shader_effect_update(float dt);
 
+        /*!
+         * \brief Applies a fragment-based lighting effect to enhance the scene's lighting dynamically.
+         *
+         * This static method enables the fragment lighting shader effect, aiming to improve the lighting
+         * quality by applying more sophisticated lighting calculations on a per-fragment basis. It sets
+         * the currentShaderEffect to fragment_light, suitable for scenes requiring enhanced lighting
+         * details or atmospheric effects.
+         */
+        static void play_fragment_light();
+
+        /*!
+         * \brief Updates the active shader effect based on elapsed time.
+         *
+         * This static method is responsible for updating the parameters or intensity of the currently
+         * active shader effect, utilizing the delta time to ensure smooth transitions and animations.
+         * The method adapts the effect's behavior over time, allowing for dynamic visual experiences.
+         *
+         * \param dt The delta time since the last update, used to adjust effect parameters over time.
+         */
+        static void shader_effect_update(float dt);
 
         static shaderEffect currentShaderEffect;
         static float shader_effect_timer;
