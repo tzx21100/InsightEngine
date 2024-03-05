ad002c886e52a21bd41e95ae4628f9cd2cb65c08
macoki07 <70043062+macoki07@users.noreply.github.com>
2024-03-05 17:02:18 +0800

    fixed logo on main menu

--- Insight Engine/Insight Engine/Assets/Scripts/MainMenuScript.cs
+++ Insight Engine/Insight Engine/Assets/Scripts/MainMenuScript.cs
@@ -30,8 +30,10 @@ namespace IS
         static private int settings_button_entity;
         static private int how_to_play_button_entity;
         static private int exit_button_entity;
+        static private int team_logo_entity;
 
         static SimpleImage game_title_image;
+        static SimpleImage team_logo_image;
         static SimpleImage button_frame;
 
         static Vector2D game_title_pos = new Vector2D(0, 0);
@@ -39,17 +41,20 @@ namespace IS
         static Vector2D settings_button_pos = new Vector2D(0, 0);
         static Vector2D how_to_play_button_pos = new Vector2D(0, 0);
         static Vector2D exit_button_pos = new Vector2D(0, 0);
+        static Vector2D team_logo_pos = new Vector2D(0, 0);
 
         static public void Init()
         {
             game_title_image = InternalCalls.GetSpriteImage("fragments_game_logo.png");
             button_frame = InternalCalls.GetSpriteImage("button_frame.png");
+            team_logo_image = InternalCalls.GetSpriteImage("team_logo.png");
 
             game_title_entity = InternalCalls.CreateEntityUI("Game Title", game_title_image);
-            start_button_entity = InternalCalls.CreateEntityButtonNoText("Start Button", button_frame, "StartButtonScript"); //look thru
-            settings_button_entity = InternalCalls.CreateEntityButtonNoText("Settings Button", button_frame, "SettingsScript"); //done for now
-            how_to_play_button_entity = InternalCalls.CreateEntityButtonNoText("How To Play Button", button_frame, "HowToPlayScript"); //done
-            exit_button_entity = InternalCalls.CreateEntityButtonNoText("Exit Button", button_frame, "ExitButtonScript"); //done
+            start_button_entity = InternalCalls.CreateEntityButtonNoText("Start Button", button_frame, "StartButtonScript"); 
+            settings_button_entity = InternalCalls.CreateEntityButtonNoText("Settings Button", button_frame, "SettingsScript"); 
+            how_to_play_button_entity = InternalCalls.CreateEntityButtonNoText("How To Play Button", button_frame, "HowToPlayScript"); 
+            exit_button_entity = InternalCalls.CreateEntityButtonNoText("Exit Button", button_frame, "ExitButtonScript"); 
+            team_logo_entity = InternalCalls.CreateEntityButtonNoText("Team Logod", team_logo_image, "TeamLogoScript"); 
         }
 
         static public void Update()
@@ -67,6 +72,9 @@ namespace IS
             origin.x = camera_pos.x - (win_dimension.x / 2f);
             origin.y = camera_pos.y - (win_dimension.y / 2f);
             InternalCalls.TransformSetScale(win_dimension.x, win_dimension.y);
+
+            //dimensions
+            Vector2D team_logo = new Vector2D(0.4f * win_dimension.x, 0.2f * win_dimension.x);
             Vector2D button = new Vector2D(0.14f * win_dimension.x, 0.08f * win_dimension.y);
 
             // set sizes
@@ -75,6 +83,7 @@ namespace IS
             InternalCalls.SetButtonSize(settings_button_entity, new SimpleVector2D(button.x, button.y));
             InternalCalls.SetButtonSize(how_to_play_button_entity, new SimpleVector2D(button.x, button.y));
             InternalCalls.SetButtonSize(exit_button_entity, new SimpleVector2D  (button.x, button.y));
+            InternalCalls.SetButtonSize(team_logo_entity, new SimpleVector2D  (team_logo.x, team_logo.y));
 
             // set pos
             game_title_pos.Set(origin.x + (0.33f * win_dimension.x), origin.y + (0.76f * win_dimension.y));
@@ -82,6 +91,7 @@ namespace IS
             settings_button_pos.Set(origin.x + (0.31f * win_dimension.x), origin.y + (0.46f * win_dimension.y));
             how_to_play_button_pos.Set(origin.x + (0.31f * win_dimension.x), origin.y + (0.34f * win_dimension.y));
             exit_button_pos.Set(origin.x + (0.31f * win_dimension.x), origin.y + (0.22f * win_dimension.y));
+            team_logo_pos.Set(origin.x + (0.875f * win_dimension.x), origin.y + (0.175f * win_dimension.y));
 
             if (SettingsScript.show_settings || HowToPlayScript.show_how_to_play || ExitButtonScript.exit_confirmation)
             {
@@ -106,6 +116,7 @@ namespace IS
             InternalCalls.TransformSetPositionEntity(settings_button_pos.x, settings_button_pos.y, settings_button_entity);
             InternalCalls.TransformSetPositionEntity(how_to_play_button_pos.x, how_to_play_button_pos.y, how_to_play_button_entity);
             InternalCalls.TransformSetPositionEntity(exit_button_pos.x, exit_button_pos.y, exit_button_entity);
+            InternalCalls.TransformSetPositionEntity(team_logo_pos.x, team_logo_pos.y, team_logo_entity);
 
             InternalCalls.RenderTextFont("START", "MedusaGothic_D", 0.3118f, 0.583f, 7.5f, (1f, 1f, 1f, 1f));
             InternalCalls.RenderTextFont("SETTINGS", "MedusaGothic_D", 0.3118f, 0.463f, 7.5f, (1f, 1f, 1f, 1f));
@@ -121,6 +132,7 @@ namespace IS
             InternalCalls.TransformSetPositionEntity(9999f, 9999f, settings_button_entity);
             InternalCalls.TransformSetPositionEntity(9999f, 9999f, how_to_play_button_entity);
             InternalCalls.TransformSetPositionEntity(9999f, 9999f, exit_button_entity);
+            InternalCalls.TransformSetPositionEntity(9999f, 9999f, team_logo_entity);
         }
     }
 }
