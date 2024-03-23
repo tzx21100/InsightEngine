80c06b99805f747093c92d04824c552fec12c9bc
macoki07 <70043062+macoki07@users.noreply.github.com>
2024-03-23 16:30:37 +0800

    credits info done

--- Insight Engine/Insight Engine/Assets/Scripts/CreditsScript.cs
+++ Insight Engine/Insight Engine/Assets/Scripts/CreditsScript.cs
@@ -13,6 +13,7 @@
  * consent of DigiPen Institute of Technology is prohibited.
  *____________________________________________________________________________*/
 using System;
+using System.Collections.Generic;
 using System.Runtime.CompilerServices;
 namespace IS
 {
@@ -36,19 +37,59 @@ namespace IS
         static private float offset;
         static private float lineBreak;
         static private float releasedYPos;
+
+        static private List<(string name, string role)> teamMembers = new List<(string name, string role)>();
+        static private List<string> instructors = new List<string>();
+        static private List<string> boardOfDirectors = new List<string>();
+        static private List<string> executives = new List<string>();
         static public void Init()
         {
             timer = 0f;
             dt = InternalCalls.GetDeltaTime();
-            bottomY = 3.1f; // End position for the text (above the top of the screen)
+            bottomY = 5f; // End position for the text (above the top of the screen)
             topY = -0.1f; // Top position for the text (below the bottom of the screen)
-            duration = 10f;
+            duration = 40f;
             normalizedYPos = 0f;
             t = 0f;
 
             //offsets
             offset = 0.05f;
             lineBreak = offset;
+
+            teamMembers.Clear();
+            instructors.Clear();
+            boardOfDirectors.Clear();
+            executives.Clear();
+
+            teamMembers.Add(("Guo Yiming", "Programmer"));
+            teamMembers.Add(("Koh Yan Khang", "Product Manager"));
+            teamMembers.Add(("Ng De En Matthew", "Programmer"));
+            teamMembers.Add(("Tan Zheng Xun", "Technical Lead"));
+            teamMembers.Add(("Wu Zekai", "Programmer"));
+            teamMembers.Add(("Ee Tze Rong Cheryl", "Design Lead"));
+            teamMembers.Add(("Lim Wan Jin", "Art Lead"));
+            teamMembers.Add(("Ang Qin Tian Rachel", "Artist"));
+
+            instructors.Add("Elie Hosry");
+            instructors.Add("Goh Dian Yang");
+            instructors.Add("Lee Chee Yuan Alwyn");
+            instructors.Add("Goh Jing Ying");
+            instructors.Add("Danielle Fong Sum Yee");
+            instructors.Add("Holger Liebnitz");
+            instructors.Add("Vuk Krakovic");
+            instructors.Add("Gavin Parker");
+            instructors.Add("Keh Choon Wee");
+
+            boardOfDirectors.Add("Chu Jason Yeu Tat");
+            boardOfDirectors.Add("Michele Comair");
+            boardOfDirectors.Add("Christopher Comair");
+            boardOfDirectors.Add("Alexander Comair");
+
+            executives.Add("Michael Gats");
+            executives.Add("Tan Chek Ming");
+            executives.Add("Prasanna Kumar Ghali");
+            executives.Add("Mandy Wong");
+            executives.Add("Johnny Deek");
         }
 
         static public void Update()
@@ -89,55 +130,50 @@ namespace IS
             InternalCalls.RenderTextFont("FRAGMENTS", "MedusaGothic_D", 0.5f, normalizedYPos, 15f, (1f, 1f, 1f, 1f));
             InternalCalls.RenderTextFont("By Insight", "Semplicita_Light", 0.5f, normalizedYPos - lineBreak, 18f, (1f, 1f, 1f, 1f));
             
-            InternalCalls.RenderTextFont("TEAM", "MedusaGothic_D", 0.5f, normalizedYPos - lineBreak * 3f, 15f, (1f, 1f, 1f, 1f));
-            InternalCalls.RenderTextFont("Guo Yiming", "Semplicita_Light", 0.25f, normalizedYPos - lineBreak * 4f, 18f, (1f, 1f, 1f, 1f));
-            InternalCalls.RenderTextFont("Programmer", "Semplicita_Light", 0.75f, normalizedYPos - lineBreak * 4f, 18f, (1f, 1f, 1f, 1f));
-            InternalCalls.RenderTextFont("Koh Yan Khang", "Semplicita_Light", 0.25f, normalizedYPos - lineBreak * 5.5f, 18f, (1f, 1f, 1f, 1f));
-            InternalCalls.RenderTextFont("Product Manager", "Semplicita_Light", 0.75f, normalizedYPos - lineBreak * 5.5f, 18f, (1f, 1f, 1f, 1f));
-            InternalCalls.RenderTextFont("Ng De En Matthew", "Semplicita_Light", 0.25f, normalizedYPos - lineBreak * 7f, 18f, (1f, 1f, 1f, 1f));
-            InternalCalls.RenderTextFont("Programmer", "Semplicita_Light", 0.75f, normalizedYPos - lineBreak * 7f, 18f, (1f, 1f, 1f, 1f));
-            InternalCalls.RenderTextFont("Tan Zheng Xun", "Semplicita_Light", 0.25f, normalizedYPos - lineBreak * 8.5f, 18f, (1f, 1f, 1f, 1f));
-            InternalCalls.RenderTextFont("Technical Lead", "Semplicita_Light", 0.75f, normalizedYPos - lineBreak * 8.5f, 18f, (1f, 1f, 1f, 1f));
-            InternalCalls.RenderTextFont("Wu Zekai", "Semplicita_Light", 0.25f, normalizedYPos - lineBreak * 10f, 18f, (1f, 1f, 1f, 1f));
-            InternalCalls.RenderTextFont("Programmer", "Semplicita_Light", 0.75f, normalizedYPos - lineBreak * 10f, 18f, (1f, 1f, 1f, 1f));
-            InternalCalls.RenderTextFont("Ee Tze Rong Cheryl", "Semplicita_Light", 0.25f, normalizedYPos - lineBreak * 11.5f, 18f, (1f, 1f, 1f, 1f));
-            InternalCalls.RenderTextFont("Design Lead", "Semplicita_Light", 0.75f, normalizedYPos - lineBreak * 11.5f, 18f, (1f, 1f, 1f, 1f));
-            InternalCalls.RenderTextFont("Lim Wan Jin", "Semplicita_Light", 0.25f, normalizedYPos - lineBreak * 13f, 18f, (1f, 1f, 1f, 1f));
-            InternalCalls.RenderTextFont("Art Lead", "Semplicita_Light", 0.75f, normalizedYPos - lineBreak * 13f, 18f, (1f, 1f, 1f, 1f));
-            InternalCalls.RenderTextFont("Ang Qin Tian Rachel", "Semplicita_Light", 0.25f, normalizedYPos - lineBreak * 14.5f, 18f, (1f, 1f, 1f, 1f));
-            InternalCalls.RenderTextFont("Artist", "Semplicita_Light", 0.75f, normalizedYPos - lineBreak * 14.5f, 18f, (1f, 1f, 1f, 1f));
-        
-            InternalCalls.RenderTextFont("Instructors", "MedusaGothic_D", 0.5f, normalizedYPos - lineBreak * 16.5f, 15f, (1f, 1f, 1f, 1f));
-            InternalCalls.RenderTextFont("Elie Hosry", "Semplicita_Light", 0.5f, normalizedYPos - lineBreak * 18f, 18f, (1f, 1f, 1f, 1f));
-            InternalCalls.RenderTextFont("Goh Dian Yang", "Semplicita_Light", 0.5f, normalizedYPos - lineBreak * 19.5f, 18f, (1f, 1f, 1f, 1f));
-            InternalCalls.RenderTextFont("Lee Chee Yuan Alwyn", "Semplicita_Light", 0.5f, normalizedYPos - lineBreak * 21f, 18f, (1f, 1f, 1f, 1f));
-            InternalCalls.RenderTextFont("Goh Jing Ying", "Semplicita_Light", 0.5f, normalizedYPos - lineBreak * 22.5f, 18f, (1f, 1f, 1f, 1f));
-            InternalCalls.RenderTextFont("Danielle Fong Sum Yee", "Semplicita_Light", 0.5f, normalizedYPos - lineBreak * 24f, 18f, (1f, 1f, 1f, 1f));
-            InternalCalls.RenderTextFont("Holger Liebnitz", "Semplicita_Light", 0.5f, normalizedYPos - lineBreak * 25.5f, 18f, (1f, 1f, 1f, 1f));
-            InternalCalls.RenderTextFont("Vuk Krakovic", "Semplicita_Light", 0.5f, normalizedYPos - lineBreak * 27f, 18f, (1f, 1f, 1f, 1f));
-            InternalCalls.RenderTextFont("Gavin Parker", "Semplicita_Light", 0.5f, normalizedYPos - lineBreak * 28.5f, 18f, (1f, 1f, 1f, 1f));
-            InternalCalls.RenderTextFont("Keh Choon Wee", "Semplicita_Light", 0.5f, normalizedYPos - lineBreak * 30f, 18f, (1f, 1f, 1f, 1f));
+            InternalCalls.RenderTextFont("TEAM", "MedusaGothic_D", 0.5f, normalizedYPos - lineBreak * 4f, 15f, (1f, 1f, 1f, 1f));
+            float teamMemberLineBreak = 5.5f;
+            foreach (var member in teamMembers)
+            {
+                InternalCalls.RenderTextFont(member.name, "Semplicita_Light", 0.35f, normalizedYPos - lineBreak * teamMemberLineBreak, 18f, (1f, 1f, 1f, 1f));
+                InternalCalls.RenderTextFont(member.role, "Semplicita_Light", 0.65f, normalizedYPos - lineBreak * teamMemberLineBreak, 18f, (1f, 1f, 1f, 1f));
+                teamMemberLineBreak += 1.5f;
+            }
 
-            InternalCalls.RenderTextFont("Created At", "MedusaGothic_D", 0.5f, normalizedYPos - lineBreak * 32f, 15f, (1f, 1f, 1f, 1f));
-            InternalCalls.RenderTextFont("Digipen Insitute of Technology Singapore", "Semplicita_Light", 0.5f, normalizedYPos - lineBreak * 33.5f, 18f, (1f, 1f, 1f, 1f));
+            InternalCalls.RenderTextFont("Instructors", "MedusaGothic_D", 0.5f, normalizedYPos - lineBreak * (teamMemberLineBreak + 2f), 15f, (1f, 1f, 1f, 1f));
 
-            InternalCalls.RenderTextFont("President", "MedusaGothic_D", 0.5f, normalizedYPos - lineBreak * 35.5f, 15f, (1f, 1f, 1f, 1f));
-            InternalCalls.RenderTextFont("Claude Comair", "Semplicita_Light", 0.5f, normalizedYPos - lineBreak * 37f, 18f, (1f, 1f, 1f, 1f));
+            float instructorLineBreak = teamMemberLineBreak + 3.5f;
+            foreach (string instructor in instructors)
+            {
+                InternalCalls.RenderTextFont(instructor, "Semplicita_Light", 0.5f, normalizedYPos - lineBreak * instructorLineBreak, 18f, (1f, 1f, 1f, 1f));
+                instructorLineBreak += 1.5f;
+            }
 
-            InternalCalls.RenderTextFont("Board of Directors", "MedusaGothic_D", 0.5f, normalizedYPos - lineBreak * 39f, 15f, (1f, 1f, 1f, 1f));
-            InternalCalls.RenderTextFont("Chu Jason Yeu Tat", "Semplicita_Light", 0.5f, normalizedYPos - lineBreak * 40.5f, 18f, (1f, 1f, 1f, 1f));
-            InternalCalls.RenderTextFont("Michele Comair", "Semplicita_Light", 0.5f, normalizedYPos - lineBreak * 42f, 18f, (1f, 1f, 1f, 1f));
-            InternalCalls.RenderTextFont("Christopher Comair", "Semplicita_Light", 0.5f, normalizedYPos - lineBreak * 43.5f, 18f, (1f, 1f, 1f, 1f));
-            InternalCalls.RenderTextFont("Alexander Comair", "Semplicita_Light", 0.5f, normalizedYPos - lineBreak * 45f, 18f, (1f, 1f, 1f, 1f));
+            InternalCalls.RenderTextFont("Created At", "MedusaGothic_D", 0.5f, normalizedYPos - lineBreak * (instructorLineBreak + 1.5f), 15f, (1f, 1f, 1f, 1f));
+            InternalCalls.RenderTextFont("Digipen Insitute of Technology Singapore", "Semplicita_Light", 0.5f, normalizedYPos - lineBreak * (instructorLineBreak + 3f), 18f, (1f, 1f, 1f, 1f));
 
-            InternalCalls.RenderTextFont("Executives", "MedusaGothic_D", 0.5f, normalizedYPos - lineBreak * 47f, 15f, (1f, 1f, 1f, 1f));
-            InternalCalls.RenderTextFont("Michael Gats", "Semplicita_Light", 0.5f, normalizedYPos - lineBreak * 48.5f, 18f, (1f, 1f, 1f, 1f));
-            InternalCalls.RenderTextFont("Tan Chek Ming", "Semplicita_Light", 0.5f, normalizedYPos - lineBreak * 50f, 18f, (1f, 1f, 1f, 1f));
-            InternalCalls.RenderTextFont("Prasanna Kumar Ghali", "Semplicita_Light", 0.5f, normalizedYPos - lineBreak * 51.5f, 18f, (1f, 1f, 1f, 1f));
-            InternalCalls.RenderTextFont("Mandy Wong", "Semplicita_Light", 0.5f, normalizedYPos - lineBreak * 53f, 18f, (1f, 1f, 1f, 1f));
-            InternalCalls.RenderTextFont("Johnny Deek", "Semplicita_Light", 0.5f, normalizedYPos - lineBreak * 54.5f, 18f, (1f, 1f, 1f, 1f));
+            InternalCalls.RenderTextFont("President", "MedusaGothic_D", 0.5f, normalizedYPos - lineBreak * (instructorLineBreak + 6f), 15f, (1f, 1f, 1f, 1f));
+            InternalCalls.RenderTextFont("Claude Comair", "Semplicita_Light", 0.5f, normalizedYPos - lineBreak * (instructorLineBreak + 7.5f), 18f, (1f, 1f, 1f, 1f));
 
-            //InternalCalls.RenderTextFont("Created At", "MedusaGothic_D", 0.5f, normalizedYPos - lineBreak * 32f, 15f, (1f, 1f, 1f, 1f));
+
+            InternalCalls.RenderTextFont("Board of Directors", "MedusaGothic_D", 0.5f, normalizedYPos - lineBreak * (instructorLineBreak + 10.5f), 15f, (1f, 1f, 1f, 1f));
+            float bodLineBreak = instructorLineBreak + 12f;
+            foreach (string bod in boardOfDirectors)
+            {
+                InternalCalls.RenderTextFont(bod, "Semplicita_Light", 0.5f, normalizedYPos - lineBreak * bodLineBreak, 18f, (1f, 1f, 1f, 1f));
+                bodLineBreak += 1.5f;
+            }
+
+            InternalCalls.RenderTextFont("Executives", "MedusaGothic_D", 0.5f, normalizedYPos - lineBreak * (bodLineBreak + 1.5f), 15f, (1f, 1f, 1f, 1f));
+            
+            float execLineBreak = bodLineBreak + 3f;
+            foreach (string exec in executives)
+            {
+                InternalCalls.RenderTextFont(exec, "Semplicita_Light", 0.5f, normalizedYPos - lineBreak * execLineBreak, 18f, (1f, 1f, 1f, 1f));
+                execLineBreak += 1.5f;
+            }
+
+            InternalCalls.RenderTextFont("www.digipen.edu", "Semplicita_Light", 0.5f, normalizedYPos - lineBreak * (execLineBreak + .5f), 9f, (1f, 1f, 1f, 1f));
+            InternalCalls.RenderTextFont("All content 2024 DigiPen Institute of Technology Singapore. All Rights Reserved.", "Semplicita_Light", 0.5f, normalizedYPos - lineBreak * (execLineBreak + 1.25f), 9f, (1f, 1f, 1f, 1f));
 
 
             timer += dt;
