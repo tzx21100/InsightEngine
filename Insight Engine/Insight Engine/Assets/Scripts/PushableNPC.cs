4f60bebdb03682133702380d0d6c580c051a9a37
tzx21100 <115536100+tzx21100@users.noreply.github.com>
2024-04-06 16:52:38 +0800

    red>red ending

--- Insight Engine/Insight Engine/Assets/Scripts/PushableNPC.cs
+++ Insight Engine/Insight Engine/Assets/Scripts/PushableNPC.cs
@@ -9,8 +9,12 @@ namespace IS
         static bool happy_activated=false;
         static float tint_timer = 2f;
         static float tint_timer_set = 2f;
+        static float tint_away = 2f;
+
+
         static public void Init(){
             tint_timer = tint_timer_set;
+            tint_away = 0f;
             createdLastFragment = false;
             textshow=false;
             TextBox.Init();
@@ -36,18 +40,45 @@ namespace IS
                 if (tint_timer > 0f)
                 {
                     tint_timer -= InternalCalls.GetDeltaTime();
-                    InternalCalls.DrawImageAt(new SimpleVector2D(0, 0), 0, new SimpleVector2D(3840, 2160), InternalCalls.GetSpriteImage("credits_bg.jpg"), 1f,6);
-                    float alpha = 1 - tint_timer / 2f;
-                    if (alpha < 0.5f) { alpha = 0.5f; }
+                    InternalCalls.DrawImageAt(new SimpleVector2D(0, 0), 0, new SimpleVector2D(InternalCalls.GetWindowHeight()*3840 /2160, InternalCalls.GetWindowHeight()), InternalCalls.GetSpriteImage("credits_bg.jpg"), 1f,6);
                     InternalCalls.SetLightsToggle(false);
-                    InternalCalls.DrawSquare(0, 0, 20000, 20000, 0, 0, 0, alpha, 6);
                     CameraScript.CameraPanTo(new Vector2D(0, 0), 12f);
-                    CameraScript.CameraTargetZoom(0.5f, 0.5f);
+                    InternalCalls.AttachCamera(0, 0);
+                    CameraScript.CameraTargetZoom(1f, 0.5f);
+                    if (tint_timer <= 0f)
+                    {
+                        TextBox.CreateTextBox("In your courageous sacrifice to become the final fragment to this broken world.");
+                        TextBox.AddTextLines("You redeemed your loved ones, and tranquility returned to the village.");
+                        TextBox.AddTextLines("Yet, admist the semblance of normalcy, there lingered a bittersweet ache.");
+                        TextBox.AddTextLines("As if whispers of your presence were fading into the ether...");
+                    }
                     return;
                 }
-                CameraScript.StopCameraPan();
-                InternalCalls.SetLightsToggle(true);
-                InternalCalls.LoadScene("Assets/Scenes/CreditsHappy.insight");
+
+
+
+                InternalCalls.DrawImageAt(new SimpleVector2D(0, 0), 0, new SimpleVector2D(InternalCalls.GetWindowHeight() * 3840 / 2160, InternalCalls.GetWindowHeight()), InternalCalls.GetSpriteImage("credits_bg.jpg"), 1f, InternalCalls.GetTopLayer()+2);
+
+                CameraScript.CameraPanTo(new Vector2D(0, 0), 12f);
+                CameraScript.CameraTargetZoom(1f, 0.5f);
+
+                if (TextBox.isVisible == false)
+                {
+                    float alpha = 1f;
+                    tint_away += InternalCalls.GetDeltaTime();
+                    alpha = tint_away;
+                    if (alpha > 0.5f) { alpha = 0.5f; }
+                    InternalCalls.DrawSquare(0, 0, 20000, 20000, 0, 0, 0, alpha, InternalCalls.GetTopLayer()+2);
+                    if (tint_away > 1f) {
+                        CameraScript.StopCameraPan();
+                        InternalCalls.SetLightsToggle(true);
+                        InternalCalls.LoadScene("Assets/Scenes/CreditsHappy.insight");
+                    }
+
+
+
+                }
+
                 return;
             }
 
